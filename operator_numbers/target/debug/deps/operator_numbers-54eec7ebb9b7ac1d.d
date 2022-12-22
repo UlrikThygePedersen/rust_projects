c4c82c12313852e027087adb86372c9e46dc880d
Ulrik Pedersen <ulriktpedersen@gmail.com>
2022-12-22 11:59:33 +0100

    stuff

---
+++ operator_numbers/target/debug/deps/operator_numbers-54eec7ebb9b7ac1d.d
@@ -0,0 +1,5 @@
+c:\Users\10122055\OneDrive - NTT DATA Business Solutions\Projekter\Rust_Projects\operator_numbers\target\debug\deps\operator_numbers-54eec7ebb9b7ac1d.rmeta: src\main.rs
+
+c:\Users\10122055\OneDrive - NTT DATA Business Solutions\Projekter\Rust_Projects\operator_numbers\target\debug\deps\operator_numbers-54eec7ebb9b7ac1d.d: src\main.rs
+
+src\main.rs:
