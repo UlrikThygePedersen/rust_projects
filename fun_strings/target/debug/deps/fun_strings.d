ef5bebaa7bbbb485d9495e1149b6ac33e31883ab
Ulrik Pedersen <ulriktpedersen@gmail.com>
2022-12-22 13:44:37 +0100

    fun strings

---
+++ fun_strings/target/debug/deps/fun_strings.d
@@ -0,0 +1,5 @@
+C:\Users\10122055\OneDrive - NTT DATA Business Solutions\Projekter\Rust_Projects\fun_strings\target\debug\deps\fun_strings.exe: src\main.rs
+
+C:\Users\10122055\OneDrive - NTT DATA Business Solutions\Projekter\Rust_Projects\fun_strings\target\debug\deps\fun_strings.d: src\main.rs
+
+src\main.rs:
