47e323ac0034f748912808a80596d3c588a2baba
Ulrik Pedersen <ulriktpedersen@gmail.com>
2023-01-02 11:38:21 +0100

    polars

--- polars_test/src/main.rs
+++ polars_test/src/main.rs
@@ -1,3 +1,11 @@
+:dep polars = { version = "0.23.2", features = ["lazy", "csv-file", "strings", "temporal", "dtype-duration", "dtype-categorical", "concat_str", "list", "list_eval", "rank", "lazy_regex"]}
+:dep color-eyre = {version = "0.6.2"}
+:dep rand = {version = "0.8.5"}
+:dep reqwest = { version = "0.11.11", features = ["blocking"]}
+
+use color_eyre::{Result};
+use polars::prelude::*;
+
 fn main() {
     println!("Hello, world!");
 }
